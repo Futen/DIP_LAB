a2d07f7c5f61ea5909c72f55acbed4809de1a1c6
Futen <tdk356ubuntu@yahoo.com.tw>
2015-10-23 02:08:18 +0800

    TEST

---
+++ HistPlot.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0,256)
+y = np.zeros(256)
+f = open("hist_ori.txt",'r')
+index = 0
+for line in f:
+    line = line.split("\n")[0]
+    y[index] = int(float(line))
+    index+=1
+f.close()
+#print x
+#print y
+plt.plot(x,y)
+plt.xlabel("intensity")
+plt.ylabel("times")
+plt.title("Histogram")
+plt.show()
+f = open("hist_equ.txt",'r')
+index = 0
+for line in f:
+    line = line.split("\n")[0]
+    y[index] = int(float(line))
+    index+=1
+f.close()
+#print x
+#print y
+plt.plot(x,y)
+plt.xlabel("intensity")
+plt.ylabel("times")
+plt.title("Histogram")
+plt.show()
