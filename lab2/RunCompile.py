0fd576bb45f054317dd10946e8748810bcf99a07
Futen <tdk356ubuntu@yahoo.com.tw>
2015-10-28 01:50:53 +0800

    gg

--- lab2/RunCompile.py
+++ lab2/RunCompile.py
@@ -3,5 +3,5 @@
 import subprocess
 import glob
 
-s = 'g++   -W -I/usr/include/opencv lab2.cpp main.cpp -lm -lopencv_core -lopencv_highgui'
+s = 'g++ -W  -O3 -I/usr/include/opencv lab2.cpp main.cpp -lm -lopencv_core -lopencv_highgui'
 subprocess.call(s,shell=True)
