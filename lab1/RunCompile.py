f6e9ab60dd477eae3a1d73765cadd2dced14db3e
Futen <tdk356ubuntu@yahoo.com.tw>
2015-10-28 01:58:51 +0800

    -O3

--- lab1/RunCompile.py
+++ lab1/RunCompile.py
@@ -3,5 +3,5 @@
 import subprocess
 import glob
 
-s = 'g++  -I/usr/include/opencv test.cpp Image_Function.cpp  -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_flann'
+s = 'g++  -O3  -I/usr/include/opencv test.cpp Image_Function.cpp  -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_flann'
 subprocess.call(s,shell=True)
