f571dfe2e997e88d0e7f58e97419c5a351bd6c09
Futen <tdk356ubuntu@yahoo.com.tw>
2015-11-23 15:36:58 +0800

    t

--- lab3/task.m
+++ lab3/task.m
@@ -1,11 +1,12 @@
 
-input = imread('Fig0431(d)(blown_ic_crop).tif');
+%input = imread('Fig0431(d)(blown_ic_crop).tif');
+input = imread('Fig0424(a)(rectangle).tif');
 input = im2single(input);
 inputSize = size(input);
 a = myDFT2(input);
 %f = myGLPF(5,inputSize(1),inputSize(2));
 %a = a.*f;
-%a = abs(a);
-a = myIDFT2(a);
+a = abs(a);
+%a = myIDFT2(a);
 a = im2uint8(a);
 imshow(a);
