61ee088dd143f9c90e0cbff4efeaa5e8ed5e3906
Futen <tdk356ubuntu@yahoo.com.tw>
2015-11-23 11:12:54 +0800

    gg

--- lab3/myDFT2.m
+++ lab3/myDFT2.m
@@ -3,27 +3,27 @@ function[output] = myDFT2(image)
     imageSize = size(image); % get input image size
     width = imageSize(2); % get image size
     height = imageSize(1);
-    for row = 1:height
+    for row = 1:height  % move center point
         for col = 1:width
             image(row,col) = image(row,col)*(-1)^(row+col);
         end
     end
-
+    % create some vector to do operation
     row_linspace = (1:height);
     col_linspace = (1:width);
     row_exp_vector = (1:height)/height*2*pi;
     col_exp_vector = (1:width)/width*2*pi;
-
+    % init vector
     row_exp_space = zeros([height, height]);
     col_exp_space = zeros([width, width]);
     output = zeros([height, width]);
-
+    % do some calculation for speed up program
     for row = 1:height
         row_exp_space(row,:) = exp(complex(0,-1)*(row_exp_vector*row));
     end
     for col = 1:width
         col_exp_space(col,:) = exp(complex(0,-1)*(col_exp_vector*col));
     end
-    output = row_exp_space * image * col_exp_space';
+    output = row_exp_space * image * col_exp_space'; % the product of the matrix will be output
     toc;
 end
--- lab3/myGLPF.m
+++ lab3/myGLPF.m
@@ -1,13 +1,13 @@
 function[output] = myGLPF(D0, M, N)
     tic;
-    center = zeros([1,2]);
+    center = zeros([1,2]); % get center point
     center(1) = round((1+M)/2);
     center(2) = round((1+N)/2);
     output = zeros([M,N]);
     for row = 1:M
         for col = 1:N
-            distance = sqrt(sum(([row,col]-center).^2));
-            output(row,col) = exp(-0.5*(distance/D0)^2);
+            distance = sqrt(sum(([row,col]-center).^2)); % get distance from center
+            output(row,col) = exp(-0.5*(distance/D0)^2); % get corresponding value
         end
     end
     toc;
