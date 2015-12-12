9b17811f92015c93b5219cd6d85696b3e3205a60
Futen <tdk356@yahoo.com.tw>
2015-12-13 01:54:34 +0800

    gg

--- lab4/addSinNoise.m
+++ lab4/addSinNoise.m
@@ -6,9 +6,19 @@ function[output] = addSinNoise(input_s, A, u0, v0)
     width = inputSize(2);
     for row = 1:height
         for col = 1:width
-           mask(row,col) = A * sin((u0*col + v0*row));
+           mask(row,col) = A * cos((u0*col + v0*row)*2*pi);
         end
     end
+    %{
+    mask_tmp = zeros(inputSize);
+    for row = 1:height
+        for col = 1:width
+            mask_tmp(row,col) = mask(row,col)*(-1)^(row+col);
+        end
+    end
+    %}
+    imshow(log10(abs(myDFT2(mask))));
+    
     output = input_s + mask;
     toc;
 end
