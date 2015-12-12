a57acb211595767ef2eba139a6aa7d835d13a5c5
Futen <tdk356@yahoo.com.tw>
2015-12-12 23:16:05 +0800

    tt

--- lab4/myIDFT2.m
+++ lab4/myIDFT2.m
@@ -1,9 +1,10 @@
-function[output] = myDFT2(image)
+function[output] = myIDFT2(input)
     tic;
-    imageSize = size(image); % get input image size
+    image = conj(input);
+    imageSize = size(input); % get input image size
     width = imageSize(2); % get image size
     height = imageSize(1);
-    % create some vector to do operation
+    % create some matrix to do operation
     row_linspace = (1:height);
     col_linspace = (1:width);
     row_exp_vector = (1:height)/height*2*pi;
@@ -12,13 +13,14 @@ function[output] = myDFT2(image)
     row_exp_space = zeros([height, height]);
     col_exp_space = zeros([width, width]);
     output = zeros([height, width]);
-    % do some calculation for speed up program
+    % make some matrix to speed up program
     for row = 1:height
         row_exp_space(row,:) = exp(complex(0,-1)*(row_exp_vector*row));
     end
     for col = 1:width
         col_exp_space(col,:) = exp(complex(0,-1)*(col_exp_vector*col));
     end
-    output = row_exp_space * image * col_exp_space'; % the product of the matrix will be output
+    output = row_exp_space * image * col_exp_space'; % the produt of matrix will be output
+    output = abs(output)/(width*height);
     toc;
 end
