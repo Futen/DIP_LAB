a079e087bb2dd8c4adfa78b1e05f9c953e121f64
Futen <tdk356@yahoo.com.tw>
2015-12-16 00:49:20 +0800

    gg

--- lab4/addMotionBlur.m
+++ lab4/addMotionBlur.m
@@ -2,17 +2,19 @@ function[output_f, H] = addMotionBlur(input_f, T, a, b)
     inputSize = size(input_f);
     height = inputSize(1);
     width = inputSize(2);
-    row_line = (1:height)*b;
-    col_line = (1:width)*a;
-    two_add_space = zeros(inputSize);
+    H = zeros(inputSize);
+    center_row = (1+height)/2;
+    center_col = (1+width)/2;
     for row = 1:height
-        two_add_space(row,:) = row_line(row) + col_line;
+        for col = 1:width
+            tmp = (b*(row-center_row) + a*(col-center_col))*pi;
+            tmp = (T/tmp)*sin(tmp)*exp(-1j*tmp);
+            if isnan(tmp)
+                H(row,col) = 1;
+            else
+                H(row,col) = tmp;
+            end
+        end
     end
-    two_add_space;
-    exp_space = exp(complex(0,-1)*pi*two_add_space);
-    sin_space = sin(pi * two_add_space);
-    mul_space = (T/pi) * (two_add_space).^-1;
-    H = mul_space .* sin_space .* exp_space;
-    %imshow(abs(H));
-    output_f = input_f .* abs(H);
+    output_f = input_f .* H;
 end
