7b212796f2adced94b255d996c4608d07dfdb284
Futen <tdk356ubuntu@yahoo.com.tw>
2015-12-17 18:22:39 +0800

    gg

--- lab4/addSinNoise.m
+++ lab4/addSinNoise.m
@@ -1,27 +1,20 @@
 function[output] = addSinNoise(input_s, A, u0, v0)
     tic;
-    inputSize = size(input_s);
+    inputSize = size(input_s); % get image size
     mask = zeros(inputSize);
     height = inputSize(1);
     width = inputSize(2);
-    center_row = (1+height)/2;
-    center_col = (1+width)/2;
+    center_row = (1+height)/2; % get center of image
+    center_col = (1+width)/2;  % get center of image
     for row = 1:height
         for col = 1:width
-           mask(row,col) = A * cos((u0*col + v0*row)*2*pi);
+           mask(row,col) = A * sin((u0*col + v0*row)*2*pi); % calculate sin noise to mask
         end
     end
-    %{    
-    mask_tmp = zeros(inputSize);
-    for row = 1:height
-        for col = 1:width
-            mask_tmp(row,col) = mask(row,col)*(-1)^(row+col);
-        end
-    end
-    
-    imwrite(log10(abs(fft2(mask_tmp))), 'mask.tif');
-    %}
-    output = input_s + mask;
+    %tmp = (fft2(mask));
+    %imshow(log10(abs(tmp)));
+
+    output = input_s + mask; % add noise to origin image
     toc;
 end
 
--- lab4/notchFiltering.m
+++ lab4/notchFiltering.m
@@ -1,22 +1,22 @@
 function[output_f, Notch] = notchFiltering(input_f, D0, u0, v0)
     tic;
-    inputSize = size(input_f);
+    inputSize = size(input_f);  % get image size
     height = inputSize(1);
     width = inputSize(2);
     Notch = ones(inputSize);
-    center_row = (1+height)/2;
-    center_col = (1+width)/2;
+    center_row = (1+height)/2;  % get center
+    center_col = (1+width)/2;   % get center
 
     for row = 1:height
         for col = 1:width
-            dis_pos = sqrt((row-center_row-v0)^2 + (col-center_col-u0)^2);
-            dis_neg = sqrt((row-center_row+v0)^2 + (col-center_col+u0)^2);
-            if dis_pos <= D0 || dis_neg <= D0
+            dis_pos = sqrt((row-center_row-v0)^2 + (col-center_col-u0)^2); % calculate the radius of pos
+            dis_neg = sqrt((row-center_row+v0)^2 + (col-center_col+u0)^2); % calculate the radius of neg
+            if dis_pos <= D0 || dis_neg <= D0   % check if point is in the radius
                 Notch(row, col) = 0;
             end
         end
     end
     %Notch
-    output_f = input_f .* Notch;
+    output_f = input_f .* Notch; % add Notch to image
     toc;
 end
